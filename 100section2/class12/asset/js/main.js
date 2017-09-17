33b6a476cce7a19d30b0d76197cbdec44cdfe949
zakirhossain7 <zzakirhossain@gmail.com>
2017-09-18 00:56:10 +0600

    class12

--- 100section2/class12/asset/js/main.js
+++ 100section2/class12/asset/js/main.js
@@ -1,10 +1,3 @@
 jQuery(document).ready(function($) {
-	$(".product-carousel").owlCarousel({
-		items:4,
-		margin:30,
-		loop:true,
-		autoplay:true,
-		nav:true,
-		navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-	});
+	$(".js-modal-btn").modalVideo();
 });
